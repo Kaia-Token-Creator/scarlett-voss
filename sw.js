cc0699f9cac2108ee47531957cb0caf4892bf8b1
Kaia-Token-Creator <kaiatokencreator@gmail.com>
2025-12-14 10:32:26 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10321593
+    "domain": "5gvci.com",
+    "zoneId": 10321601
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
